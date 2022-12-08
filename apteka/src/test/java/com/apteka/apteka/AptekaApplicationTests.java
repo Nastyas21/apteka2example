d01e19cc61be7c42e07fb72165f1b122df7cefe5
Nastyas21 <nastyasvorob@gmail.com>
2022-12-08 11:38:11 +0300

    Initial commit

---
+++ apteka/src/test/java/com/apteka/apteka/AptekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apteka.apteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
