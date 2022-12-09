c2e6858d3c93750262a98b1e694cc38162e2a1ba
Nastyas21 <nastyasvorob@gmail.com>
2022-12-09 09:50:04 +0300

    version 2

--- apteka/src/main/resources/database.sql
+++ apteka/src/main/resources/database.sql
@@ -5,9 +5,31 @@ CREATE TABLE users(
 )
     ENGINE = InnoDB;
 
-CREATE TABLE role(
+CREATE TABLE roles(
                      id int not null auto_increment primary key,
                      name varchar(100) NOT NULL
 )
 
     ENGINE = InnoDB;
+
+CREATE TABLE user_roles(
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+
+    FOREIGN KEY(user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+
+    UNIQUE (user_id, role_id)
+
+
+)
+
+    ENGINE = InnoDB;
+
+INSERT INTO users VALUES (1, 'nikitaAdmin', 'adminadmin');
+INSERT INTO roles VALUES (1, 'user');
+INSERT INTO roles VALUES (2, 'admin');
+
+INSERT INTO user_roles VALUES (1,2);
+
+
